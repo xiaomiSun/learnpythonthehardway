263888c312ba10a81dc0da720d3d0a87b6251266
sunxiaomi <sunhemin@outlook.com>
2017-06-01 20:48:07 +0800

    ex1.py study drills

--- ex1.py
+++ ex1.py
@@ -5,3 +5,5 @@ print("This is fun.")
 print("Yay! Printing.")
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+# a '#' means comment out, contents after '#' will not show in script
+# Make your script print only one of the lines--comment out all except one line
